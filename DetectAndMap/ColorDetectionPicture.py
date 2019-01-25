aabce32ada199506c7a5a5a1405fbc99923875f6
Oliver Nick <on229@cam.ac.uk>
2019-01-25 14:09:54 +0000

    Changed T colors

--- DetectAndMap/ColorDetectionPicture.py
+++ DetectAndMap/ColorDetectionPicture.py
@@ -10,15 +10,13 @@ def detectColor(frame, lower, upper, minArea):
     # Threshold the HSV image to get only green colors
     mask = cv2.inRange(hsv, lower, upper)
 
-    #cv2.imshow('frame',mask)
-    #cv2.waitKey(0)
     kernel = np.ones((5,5),'int')
     dilated = cv2.dilate(mask,kernel)
     # Bitwise-AND mask and original image
     res = cv2.bitwise_and(frame,frame, mask=mask)
     ret,thrshed = cv2.threshold(cv2.cvtColor(res,cv2.COLOR_BGR2GRAY),3,255,cv2.THRESH_BINARY)
     contours,hier = cv2.findContours(thrshed,cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)
-    
+
     coord = []
     for cnt in contours:
         #Contour area is taken
@@ -35,9 +33,9 @@ fool=True
 while(fool):
     fool=False
     # Take one
-    frame = cv2.imread('lightOn2 (2).jpg')
+    frame = cv2.imread('gold5.jpg')
     #1080*1920*3
-    frame = frame[0:1080,160:1520]
+    #frame = frame[0:1080,160:1520]
     frameCopy = frame.copy()
     # define range of green color in HSV
     #use gimp for color code: 160, 20, 73 for green robot
@@ -64,19 +62,33 @@ while(fool):
     lower_cells = np.array([195/2,30*255/100,65*255/100])
     upper_cells = np.array([215/2,80*255/100,95*255/100])
 
+
+    #values gold green T
+    lower_greenR = np.array([82/2,0*255/100,50*255/100])
+    upper_greenR = np.array([135/2,27*255/100,80*255/100])
+    lower_goldR = np.array([38/2,30*255/100,94*255/100])
+    upper_goldR = np.array([57/2,68*255/100,90102*255/100])
+    lower_cells = np.array([195/2,30*255/100,65*255/100])
+    upper_cells = np.array([215/2,80*255/100,95*255/100])
+
     #TODO crop to all except safe zone
     #detect green
     #that s the robot
     #get xy and orientation
-    blueF = detectColor(frame, lower_blueR, upper_blueR, 1000)[0]
-    greenF = detectColor(frame, lower_greenR, upper_greenR, 1000)[0]
+    goldF = detectColor(frame, lower_goldR, upper_goldR, 200)
+    greenF = detectColor(frame, lower_greenR, upper_greenR, 200)
+    if len(goldF)==0 or len(greenF)==0:
+        print(str(len(goldF)) + "  " + str(len(greenF)))
+        continue
+    goldF = goldF[0]
+    greenF = greenF[0]
     #print(blueF)
     #print(greenF)
-    blueC = (blueF[0]+blueF[2]/2,blueF[1] + blueF[3]/2)
+    goldC = (goldF[0]+goldF[2]/2,goldF[1] + goldF[3]/2)
     greenC = (greenF[0]+greenF[2]/2,greenF[1] + greenF[3]/2)
 
     #angle with respect to horizontal, positive as anticlockwise. Counterintuitive sign in expression bc y axis inverted
-    alpha = math.degrees(math.atan2(-blueC[1]+greenC[1],blueC[0]-greenC[0]))
+    alpha = math.degrees(math.atan2(-goldC[1]+greenC[1],goldC[0]-greenC[0]))
     print("Angle and coordinates of the robot")
     print(alpha)
     print(greenC)
@@ -84,7 +96,7 @@ while(fool):
     #get coordinates of cells
     #crop_img = img[y:y+h, x:x+w]
     #frame2 = frameCopy[20:1060,740:1400];
-    rectMines = detectColor(frame, lower_cells, upper_cells,50);
+    rectMines = detectColor(frame, lower_cells, upper_cells,5);
     coordMines=[]
     for rect in rectMines:
         coordMines.append((rect[0]+rect[2]/2,rect[1] + rect[3]/2))
--- DetectAndMap/fullProgram.py
+++ DetectAndMap/fullProgram.py
@@ -4,7 +4,7 @@ import math
 import serial  # Serial imported for Serial communication
 import time  # Required to use delay functions
 print("Imports done")
-cap = cv2.VideoCapture(1)
+cap = cv2.VideoCapture(0)
 #ArduinoSerial = serial.Serial('com5', 9600)  # Create Serial port object called arduinoSerialData
 print("Capture started")
 time.sleep(2)  # wait for 2 secounds for the communication to get established
@@ -17,10 +17,12 @@ time.sleep(2)  # wait for 2 secounds for the communication to get established
 #area +- 70 - 260
 #172,80,60 for save zone
 #values light on(light off is more accurate, values in ColorDetectionPicture.py)
-lower_greenR = np.array([120/2,10*255/100,75*255/100])
-upper_greenR = np.array([155/2,30*255/100,100*255/100])
-lower_blueR = np.array([200/2,30*255/100,70*255/100])
-upper_blueR = np.array([225/2,75*255/100,90*255/100])
+
+    #values gold green T
+lower_greenR = np.array([82/2,0*255/100,50*255/100])
+upper_greenR = np.array([135/2,27*255/100,80*255/100])
+lower_goldR = np.array([38/2,30*255/100,94*255/100])
+upper_goldR = np.array([57/2,68*255/100,90102*255/100])
 lower_cells = np.array([195/2,30*255/100,65*255/100])
 upper_cells = np.array([215/2,80*255/100,95*255/100])
 
@@ -49,7 +51,7 @@ def detectColor(frame, lower, upper, minArea):
             #draw rectangle around found zones
             (x,y,w,h) = cv2.boundingRect(cnt)
             cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
-            coord.append((100+x,y,w,h))
+            coord.append((x,y,w,h))
     return coord
 
 firstTime = True
@@ -75,11 +77,12 @@ while(1):
         #get coordinates of cells
         #crop_img = img[y:y+h, x:x+w]
         #frame2 = frameCopy[20:1060,740:1400];
-        #cropping needed because of choice of blue rectangle on robot
-        rectMines = detectColor(frame[0:480,100:560], lower_cells, upper_cells,5);
+        #cropping needed because of choice of blue rectangle on robot not anymore #[0:480,100:560]
+        rectMines = detectColor(frame, lower_cells, upper_cells,5);
         for rect in rectMines:
             coordMines.append((rect[0]+rect[2]/2,rect[1] + rect[3]/2))
 
+        #starts at lowest y and goes up
         coordMines = sorted(coordMines,key=lambda x:(x[1],x[0]))
 
         print("Coordinates of cells")
@@ -90,19 +93,20 @@ while(1):
     #detect green
     #that s the robot
     #get xy and orientation
-    blueF = detectColor(frame, lower_blueR, upper_blueR, 200)
+    goldF = detectColor(frame, lower_goldR, upper_goldR, 200)
     greenF = detectColor(frame, lower_greenR, upper_greenR, 200)
-    if len(blueF)==0 or len(greenF)==0:
+    if len(goldF)==0 or len(greenF)==0:
+        print(str(len(goldF)) + "  " + str(len(greenF)))
         continue
-    blueF = blueF[0]
+    goldF = goldF[0]
     greenF = greenF[0]
-    #print(blueF)
+    #print(goldF)
     #print(greenF)
-    blueC = (blueF[0]+blueF[2]/2,blueF[1] + blueF[3]/2)
+    goldC = (goldF[0]+goldF[2]/2,goldF[1] + goldF[3]/2)
     greenC = (greenF[0]+greenF[2]/2,greenF[1] + greenF[3]/2)
 
     #angle with respect to horizontal, positive as anticlockwise. Counterintuitive sign in expression bc y axis inverted
-    alpha = math.degrees(math.atan2(-blueC[1]+greenC[1],blueC[0]-greenC[0]))
+    alpha = math.degrees(math.atan2(-goldC[1]+greenC[1],goldC[0]-greenC[0]))
     print("Angle and coordinates of the robot")
     print(alpha)
     print(greenC)
@@ -128,7 +132,7 @@ while(1):
         PD = kd * deriv + kp * errorAngle 
         #transform value to 0 to 200
         print("Value passed to Arduino")
-        print(PD)
+        print(100 + 200*PD) #thus 100 means straight, and above 100 is to left
         #ArduinoSerial.write(PD)
     time.sleep(0.2)
 
@@ -136,10 +140,12 @@ while(1):
     #if ArduinoSerial.readline().decode('utf-8') == 0:
     #cellsChecked += 1
 
-    #cv2.imshow('frame',frame)
-    #cv2.waitKey(0)
     deltaT = time.time()-timeOld
     timeOld = time.time()
+
+
+    #cv2.imshow('frame',frame)
+    #cv2.waitKey(0)
 
 cap.release()
 cv2.destroyAllWindows()
