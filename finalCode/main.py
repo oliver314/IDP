69c6ef7933e1adc6e779b8f5a96556ab4950d639
Oliver Barnard <obarnard99@gmail.com>
2019-02-17 15:03:46 +0000

    began updating comments

--- finalCode/main.py
+++ finalCode/main.py
@@ -1,6 +1,6 @@
 # Import required libraries
 import numpy as np
-import time  # Required to use delay functions
+import time
 import keyboard
 from TransferProtocol import Transfer
 from ImageProcessing import Imaging
@@ -16,16 +16,7 @@ uc = np.array([215 / 2, 80 * 255 / 100, 95 * 255 / 100])
 startZone = (30, 20)
 safeZone = (90, 270)
 
-'''
-TODO
-
-if detects mine and is 45 degree scew: runs into wall indefinetely
-
-start with open field or handle mine in way at start correctly
-
-'''
-
-
+# main code
 if __name__ == "__main__":
     img = Imaging(lg, ug, lp, up, lc, uc)  # Initialise imaging class
     tp = Transfer('com5')  # Initialise transfer protocol class
@@ -35,53 +26,41 @@ if __name__ == "__main__":
     while not keyboard.is_pressed(' '):
         pass
 
-    img.updateArena()
+    img.updateArena()  # Generate array of target mine coordinates
     ctrl = Controller(img, tp, startZone, safeZone)  # Initialise controller class
     startTime = time.time()
 
+    # run subroutine for collection of cells by the rear wall
     ctrl.wallCells()
+
+    # main loop to collect remaining cells and return to the safe zone
     while img.cap.isOpened() and (not keyboard.is_pressed('q')):
+
+        # update robot coordinates
         robotCoord = img.getRobotCoordinates(ctrl.targetCoord)
 
-        # Return to safe zone condition
-        if (ctrl.mineCollectedCount > 8) or (ctrl.mineCollectedCount>0 and len(img.coordMines) == 0) or time.time() - startTime > 300:
-            print(str(ctrl.mineCollectedCount) + " " + str(len(img.coordMines))+" " + str(time.time()-startTime))
+        # return to safe zone condition
+        if (ctrl.mineCollectedCount > 8) or (
+                ctrl.mineCollectedCount > 0 and len(img.coordMines) == 0) or time.time() - startTime > 300:
             targetCoord = safeZone
 
-        # Return to start zone condition
-        elif (ctrl.mineCollectedCount == 0 and time.time() - startTime > 300) or len(img.coordMines)==0:
+        # return to start zone condition
+        elif (ctrl.mineCollectedCount == 0 and time.time() - startTime > 300) or len(img.coordMines) == 0:
             targetCoord = startZone
-            ''' EXPERIMENTAL
-            img.updateArena();
-            while img.cap.isOpened() and (not keyboard.is_pressed('q')):
-                robotCoord = img.getRobotCoordinates(ctrl.targetCoord)
-                if (ctrl.mineCollectedCount > 8) or (
-                        ctrl.mineCollectedCount > 0 and len(img.coordMines) == 0) or time.time() - startTime > 300:
-                    print(str(ctrl.mineCollectedCount) + " " + str(len(img.coordMines)) + " " + str(
-                        time.time() - startTime))
-                    targetCoord = safeZone
 
-                elif (ctrl.mineCollectedCount == 0 and time.time() - startTime > 300) or len(img.coordMines) == 0:
-                    targetCoord = startZone
-
-                else:
-                    targetCoord, dist = img.getClosestCell(robotCoord)
-                    # check whether mine captured this turn and remove it from list if so
-                    ctrl.checkMineCaptured()
-
-                # print(targetCoord)
-                ctrl.driveLoop(robotCoord, targetCoord)
-                '''
         else:
+            # set target coordinates to closest cell
             targetCoord, dist = img.getClosestCell(robotCoord)
+
             # check whether mine captured this turn and remove it from list if so
             ctrl.checkMineCaptured()
 
-        #print(targetCoord)
+        # run subroutine to determine and send drive commands to the robot
         ctrl.driveLoop(robotCoord, targetCoord)
 
     # shutdown motors
     tp.send(252)
     time.sleep(1)
+
     # shutdown OpenCV
     img.shutdown()
